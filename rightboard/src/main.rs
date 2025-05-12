6badf478bf76a9ee7971d7e0f5049f8c96793069
Dennis Xu <xudennis8@gmail.com>
2025-05-11 19:59:28 -0500

    rightboard: Set system clock to 32MHz
    Update to set the system clock to 32MHz.

--- rightboard/src/main.rs
+++ rightboard/src/main.rs
@@ -5,11 +5,21 @@ extern crate panic_halt;
 
 use embassy_executor::Spawner;
 use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_stm32::rcc::{Pll, PllDiv, PllMul, PllSource, Sysclk};
+use embassy_stm32::Config;
 use embassy_time::Timer;
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) -> ! {
-    let p = embassy_stm32::init(Default::default());
+    let mut config = Config::default();
+    config.rcc.sys = Sysclk::PLL1_R;
+    config.rcc.hsi = true;
+    config.rcc.pll = Some(Pll {
+        source: PllSource::HSI,
+        div: PllDiv::DIV2,
+        mul: PllMul::MUL4,
+    });
+    let p = embassy_stm32::init(config);
 
     let mut led = Output::new(p.PB6, Level::High, Speed::Low);
 
