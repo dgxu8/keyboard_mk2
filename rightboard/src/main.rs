b37d4876c911847dda5057540df2e60f6ba0b2b2
Dennis Xu <xudennis8@gmail.com>
2025-05-13 21:31:25 -0500

    rightboard: Add usart

--- rightboard/src/main.rs
+++ rightboard/src/main.rs
@@ -6,8 +6,13 @@ extern crate panic_halt;
 use embassy_executor::Spawner;
 use embassy_stm32::gpio::{Input, Level, Output, Pull, Speed};
 use embassy_stm32::rcc::{Pll, PllDiv, PllMul, PllSource, Sysclk};
-use embassy_stm32::Config;
+use embassy_stm32::usart::{RingBufferedUartRx, Uart};
+use embassy_stm32::{bind_interrupts, peripherals, usart, Config};
 use embassy_time::{block_for, Duration, Timer};
+
+bind_interrupts!(struct Irqs {
+    USART2 => usart::InterruptHandler<peripherals::USART2>;
+});
 
 fn set_state(decoder: &mut [Output; 3], enable: &mut Output, idx: u8) {
     enable.set_low();
@@ -57,16 +62,40 @@ async fn main(_spawner: Spawner) -> ! {
         Input::new(p.PA15, Pull::Down),
     ];
 
-
     // Configure LEDs
-    let mut led0 = Output::new(p.PB6, Level::High, Speed::Low);
-    let mut led1 = Output::new(p.PB5, Level::High, Speed::Low);
+    let mut led0 = Output::new(p.PB6, Level::Low, Speed::Low);
+    let mut led1 = Output::new(p.PB5, Level::Low, Speed::Low);
 
+    // Configure usart default:
+    // baudrate: 115200,
+    // data_bits: DataBits::DataBits8,
+    // stop_bits: StopBits::STOP1,
+    // parity: Parity::ParityNone,
+    let uart = Uart::new(
+        p.USART2,
+        p.PA3, p.PA2,  // RX, TX
+        Irqs,
+        p.DMA1_CH4, p.DMA1_CH5,  // TX, RX
+        usart::Config::default()
+    ).unwrap();
+    let (mut uart_tx, uart_rx) = uart.split();
+
+    uart_tx.write(b"Test\r\n").await.unwrap();
     led0.set_high();
-    led1.set_high();
 
-    let mut bits: u8 = 0;
+    const RX_BUF_SIZE: usize = 16;
+    let mut rx_buf: [u8; RX_BUF_SIZE] = [0; RX_BUF_SIZE];
+    let mut uart_rx: RingBufferedUartRx = uart_rx.into_ring_buffered(&mut rx_buf);
+
+    // Need to use singleton!() if rx_buf goes out of scope but uart_rx is still needed, below
+    // use cortex_m::singleton;
+    // let rx_buf: &mut [u8; RX_BUF_SIZE] = singleton!(: [u8; RX_BUF_SIZE] = [0; RX_BUF_SIZE]).unwrap();
+    // let uart_rx = uart_rx.into_ring_buffered(rx_buf);
+
+    uart_rx.start_uart();
     loop {
+        let mut buffer: [u8; 1] = [0; 1];
+        let mut bits: u8 = 0;
         Timer::after_millis(5).await;
         for i in 0..8 {
             set_state(&mut decoder, &mut decoder_en, i);
@@ -74,10 +103,12 @@ async fn main(_spawner: Spawner) -> ! {
             bits |= scan_inputs(&mut inputs);
         }
         if bits > 0 {
-            led0.set_high();
+            led1.set_high();
         } else {
-            led0.set_low();
+            led1.set_low();
         }
-        bits = 0;
+        if uart_rx.read(&mut buffer).await.unwrap() > 0 {
+            uart_tx.write(&buffer).await.unwrap();
+        }
     }
 }
